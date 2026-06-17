8a1a6ed6d37c52e7619f54af6bd7c6f51a55c2d5
Sergey <butovx@gmail.com>
2026-06-17 16:37:00 +0300

    feat: build product view detail dashboard page

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+import Link from "next/link";
+import { Product } from "@/types";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowDownTrayIcon,
+} from "@heroicons/react/24/outline";
+import ProductImage from "@/components/ProductImage";
+import React from "react";
+import ProductDisplay from "@/components/product/ProductDisplay";
+
+interface ProductPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  // Use await to access params
+  const id = (await params).id;
+
+  return (
+    <Suspense
+      fallback={
+        <div className="py-8 text-center">
+          <div className="inline-block animate-spin h-8 w-8 border-4 border-blue-500 border-t-transparent rounded-full"></div>
+          <p className="mt-2 text-gray-600">Загрузка...</p>
+        </div>
+      }
+    >
+      <ProductDisplay productId={id} />
+    </Suspense>
+  );
+}
